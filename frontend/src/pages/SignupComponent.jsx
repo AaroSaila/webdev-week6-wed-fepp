d3251448db0084a6446993a1ac7fb04869d78bb9
0x6A4B <0x6a4b@proton.me>
2024-11-27 10:08:35 +0200

    added password2 field to signupcomponent, useeffect to notice user if not matching

--- frontend/src/pages/SignupComponent.jsx
+++ frontend/src/pages/SignupComponent.jsx
@@ -1,13 +1,25 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 
 const SignupComponent = ({ setIsAuthenticated }) => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [password2, setPassword2] = useState("");
   const navigate = useNavigate();
+  const [passwordIsSame, setPasswordIsSame] = useState('');
 
   const handleSignup = async () => {
   };
+
+  
+  useEffect(() => {
+    console.log("pwd changed");
+    
+    if (password !== password2)
+      setPasswordIsSame("Passwords do not match");
+    else
+      setPasswordIsSame("");
+  },[password, password2, passwordIsSame])
 
   return (
     <div className="form-container">
@@ -29,6 +41,15 @@ const SignupComponent = ({ setIsAuthenticated }) => {
           onChange={(e) => setPassword(e.target.value)}
         />
       </label>
+      <label>
+        Retype password:
+        <input
+          type="password"
+          value={password2}
+          onChange={(e) => setPassword2(e.target.value)}
+          />
+      </label>
+          <p>{passwordIsSame}</p>
       <br />
       <button onClick={handleSignup}>Sign Up</button>
     </div>
